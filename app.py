fd8472329e2a3919f9e00d6d5c2e29c9897e7c5c
salvador_sa <salvador.airo@gmail.com>
2022-08-06 16:09:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'home'
+
+@app.route('/r1')
+def index():
+    return 'Route 1'
+    
+@app.route('/r2')
+def index():
+    return 'Route 2'
+    
+@app.route('/r3')
+def index():
+    return 'Route 3'
 
